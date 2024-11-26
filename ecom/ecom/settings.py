72e961a94aec746c8d20a17df65ebb6705fdf3aa
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2024-11-26 22:07:53 +0600

    done

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -82,13 +82,31 @@ WSGI_APPLICATION = "ecom.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "Ecom",
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'mohot',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
---
+++ readme.md
@@ -0,0 +1,13 @@
+here are the steps to run the project
+1. clone the project
+   git clone https://github.com/mahfuj-mahtab/ecom-assignment.git
+2. create a virtual environment
+  python -m venv venv
+3. activate the virtual environment
+  .\venv\Scripts\activate
+4. install the requirements
+  pip install -r requirements.txt
+5. run the project
+    python manage.py migrate
+    python manage.py runserver
+
